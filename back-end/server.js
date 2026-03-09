163590a23b05dfe508aa6b42828ea2a2ff79f9d0
felipe67r <feliperibeiro67r@gmail.com>
2026-03-09 12:19:45 -0300

    Adicionando o CORS

--- back-end/server.js
+++ back-end/server.js
@@ -1,12 +1,21 @@
 import express from 'express';
+import cors from 'cors';
 import atendimentoRoutes from './routes/atendimento.routes.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-app.use(atendimentoRoutes);
+app.get('/debug-db', async (req, res) => {
+  try {
+    res.send({ status: "Backend está vivo!" });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+app.use(atendimentoRoutes);
+app.listen(3000, '0.0.0.0', () => { 
+  console.log('Server is running on port 3000');
 });
